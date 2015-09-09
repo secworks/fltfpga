21c5e6fa84253aec41197f095bbdacb09bd9cd2f
Joachim Strömbergson <joachim@secworks.se>
2015-09-09 17:34:16 +0200

    Adding initial version of top level file of the cpu.

---
+++ cpu/src/rtl/fltfpga.v
@@ -0,0 +1,138 @@
+//======================================================================
+//
+// fltcpu.v
+// --------
+// Top level file of the fltfpga cpu.
+//
+//
+// Author: Joachim Strombergson
+// Copyright (c) 2015 Secworks Sweden AB
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or
+// without modification, are permitted provided that the following
+// conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materials provided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//======================================================================
+
+module fltcpu(
+              // Clock and reset.
+              input wire           clk,
+              input wire           reset_n,
+
+              output wire          mem_cs
+              output wire          mem_we
+              output wire [31 : 0] mem_address,
+              output wire [31 : 0] mem_data
+             );
+
+
+  //----------------------------------------------------------------
+  // Internal constant and parameter definitions.
+  //----------------------------------------------------------------
+  localparam FLTCPU_IDLE = 4'h0;
+
+
+  //----------------------------------------------------------------
+  // Registers including update variables and write enable.
+  //----------------------------------------------------------------
+  reg [31 : 0] instruction_reg;
+  reg          instruction_we;
+
+  reg [3 : 0]  fltcpu_ctrl_reg;
+  reg [3 : 0]  fltcpu_ctrl_new;
+  reg          fltcpu_ctrl_we;
+
+
+  //----------------------------------------------------------------
+  // Concurrent connectivity for ports etc.
+  //----------------------------------------------------------------
+  assign mem_cs = 0;
+  assign mem_we = 0;
+  assign mem_address = 32'h00000000;
+  assign mem_data    = 32'h00000000;
+
+
+  //----------------------------------------------------------------
+  // reg_update
+  // Update functionality for all registers in the core.
+  // All registers are positive edge triggered with asynchronous
+  // active low reset.
+  //----------------------------------------------------------------
+  always @ (posedge clk or negedge reset_n)
+    begin
+      if (!reset_n)
+        begin
+          instruction_reg <= 32'h00000000;
+          fltcpu_ctrl_reg <= FLTCPU_IDLE;
+        end
+      else
+        begin
+          if (instruction_we)
+            instruction_reg <= mem_data;
+
+          if (fltcpu_ctrl_we)
+            fltcpu_ctrl_reg <= fltcpu_ctrl_new;
+        end
+    end // reg_update
+
+
+  //----------------------------------------------------------------
+  // instruction_decoder
+  //
+  // Combinational decoder of the instruction. Basically extract
+  // possible fields from the instruction register and trigger
+  // some control signals.
+  //----------------------------------------------------------------
+  always @*
+    begin : instruction_decoder
+    end // instruction_decoder
+
+
+  //----------------------------------------------------------------
+  // fltcpu_ctrl
+  //
+  // Main control FSM of the CPU.
+  //----------------------------------------------------------------
+  always @*
+    begin : fltcpu_ctrl
+      fltcpu_ctrl_new = FLTCPU_IDLE;
+      fltcpu_ctrl_we  = 0;
+
+      case (fltcpu_ctrl_reg)
+        FLTCPU_IDLE:
+          begin
+          end
+
+        default:
+          begin
+          end
+      endcase // case (fltcpu_ctrl_reg)
+    end // fltcpu_ctrl
+
+endmodule // fltcpu
+
+//======================================================================
+// EOF fltcpu.v
+//======================================================================
